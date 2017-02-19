f999c5c15b0ac46cf17e1149b9239159502a7fb9
Alex Miller <apmiller108@yahoo.com>
2017-02-18 22:39:20 -0500

    First commit

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :phoenix_starter,
+  ecto_repos: [PhoenixStarter.Repo]
+
+# Configures the endpoint
+config :phoenix_starter, PhoenixStarter.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "U7n7jnVH20jS5t7DUuatcbA8nkkwIf44drnWE0D80zTWSMyi9qebJa+0sUmfEeoJ",
+  render_errors: [view: PhoenixStarter.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: PhoenixStarter.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
