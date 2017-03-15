1f67c2b82d850c79da71a8f0c77ead2d11e5a66e
Alex Miller <apmiller108@yahoo.com>
2017-03-15 09:34:21 -0400

    Capture new name arg. Add TODOs

--- rename.sh
+++ rename.sh
@@ -2,14 +2,18 @@
 
 set -e
 
+NAME=$1
+
+# TODO: assume snake case name. Derive module name. Confirm name with user
+
 CURRENT_NAME="PhoenixStarter"
 CURRENT_OTP="phoenix_starter"
 
-NEW_NAME="NewName"
-NEW_OTP="new_name"
+# NEW_NAME="NewName"
+# NEW_OTP="new_name"
 
-ack -l $CURRENT_NAME | xargs sed -i '' -e "s/$CURRENT_NAME/$NEW_NAME/g"
-ack -l $CURRENT_OTP | xargs sed -i '' -e "s/$CURRENT_OTP/$NEW_OTP/g"
+# ack -l $CURRENT_NAME | xargs sed -i '' -e "s/$CURRENT_NAME/$NEW_NAME/g"
+# ack -l $CURRENT_OTP | xargs sed -i '' -e "s/$CURRENT_OTP/$NEW_OTP/g"
 
-mv lib/$CURRENT_OTP lib/$NEW_OTP
-mv lib/$CURRENT_OTP.ex lib/$NEW_OTP.ex
+# mv lib/$CURRENT_OTP lib/$NEW_OTP
+# mv lib/$CURRENT_OTP.ex lib/$NEW_OTP.ex
